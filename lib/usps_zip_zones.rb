7a222e66b6034e6f72c2c42d673febd38d334a5f
Alexey Naumov <alexsnaumov@gmail.com>
2019-03-28 23:34:27 +0300

    Add executable console, fix varable naming

--- lib/usps_zip_zones.rb
+++ lib/usps_zip_zones.rb
@@ -1,4 +1,6 @@
 require 'active_record'
 require_relative 'models/normal_zone'
 require_relative 'models/exception_zone'
-# connect?
+require_relative 'zone_calculator'
+db_config = Psych.load(File.open(File.expand_path('../../db/config.yml', __FILE__)))
+ActiveRecord::Base.establish_connection(db_config)
--- lib/zone_calculator.rb
+++ lib/zone_calculator.rb
@@ -1,12 +1,9 @@
-module UspsZipZones
-end
-
 class ZoneCalculator
   InvalidZip = Class.new StandardError
 
   def self.call(origin_zip, destination_zip)
-    zone = UspsZipZones::NormalZone.by_zips!(origin_zip, description_zip)
-    return zone.value unless distance.exception?
+    zone = UspsZipZones::NormalZone.by_zips!(origin_zip, destination_zip)
+    return zone.value unless zone.exception?
 
     zone = UspsZipZones::ExceptionZone.by_zips!(origin_zip, destination_zip)
     zone.value
